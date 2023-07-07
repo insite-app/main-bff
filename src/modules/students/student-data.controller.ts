7c9260e508929d6e9c51673055504ff5307ccafa
astradzhao <42476976+astradzhao@users.noreply.github.com>
2023-07-07 14:30:00 -0500

    allow frontend to access backend

--- src/modules/students/student-data.controller.ts
+++ src/modules/students/student-data.controller.ts
@@ -3,7 +3,7 @@ import { Student } from 'src/entities/students/student.entity';
 import StudentDataService from 'src/modules/students/student-data.service';
 
 @Controller('students')
-export class StudentController {
+export class StudentDataController {
   constructor(private readonly studentDataService: StudentDataService) {}
 
   @Get()
